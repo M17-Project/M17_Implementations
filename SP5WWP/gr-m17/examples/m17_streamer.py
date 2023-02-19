dbb14a51e28408b7e86d32cf1f75682e63d8a42e
Jean-Michel Friedt <friedtj@free.fr>
2023-02-19 07:35:58 +0100

    functional GNU Radio 3.10 implementation

--- SP5WWP/gr-m17/examples/m17_streamer.py
+++ SP5WWP/gr-m17/examples/m17_streamer.py
@@ -32,7 +32,7 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import m17
+from gnuradio import m17
 
 
 
