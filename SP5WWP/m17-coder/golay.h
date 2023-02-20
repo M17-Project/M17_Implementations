cad279a3ebfa5ac48894827fa9d943fd267c61e1
Jean-Michel Friedt <friedtj@free.fr>
2023-02-20 20:15:34 +0100

    make compilation compatible with C++ and global variable local

--- SP5WWP/m17-coder/golay.h
+++ SP5WWP/m17-coder/golay.h
@@ -1,6 +1,12 @@
 #ifndef GOLAY_H
 #define GOLAY_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 uint32_t golay24_encode(const uint16_t data);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- SP5WWP/m17-decoder/golay.h
+++ SP5WWP/m17-decoder/golay.h
@@ -1,6 +1,9 @@
 #ifndef GOLAY_H
 #define GOLAY_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 void IntToSoft(uint16_t* out, const uint16_t in, uint8_t len);
 uint16_t SoftToInt(const uint16_t* in, uint8_t len);
 uint16_t Div16(uint16_t a, uint16_t b);
@@ -11,5 +14,8 @@ uint32_t spopcount(const uint16_t* in, uint8_t siz);
 void calcChecksumS(uint16_t* out, const uint16_t* value);
 uint32_t SdetectErrors(const uint16_t* codeword);
 uint16_t golay24_sdecode(const uint16_t* codeword);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
