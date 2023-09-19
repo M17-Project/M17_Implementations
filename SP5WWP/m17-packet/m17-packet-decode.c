939c89b5160724a1332b81a7aad849922e06fed0
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2023-09-19 19:13:36 +0200

    [WIP] packet decoder
    multiframe text messages should now be decodable
    bugs may exist in the part of the code where chunks of frame data are joined together

--- SP5WWP/m17-packet/m17-packet-decode.c
+++ SP5WWP/m17-packet/m17-packet-decode.c
@@ -21,9 +21,11 @@ uint16_t d_soft_bit[2*SYM_PER_PLD]; //deinterleaved soft bits
 
 uint8_t lsf[30+1];                  //complete LSF (one byte extra needed for the Viterbi decoder)
 uint8_t frame_data[26+1];           //decoded frame data, 206 bits, plus 4 flushing bits
+uint8_t packet_data[33*25];         //whole packet data
 
 uint8_t syncd=0;                    //syncword found?
 uint8_t fl=0;                       //Frame=0 of LSF=1
+int8_t last_fn;                     //last received frame number (-1 when idle)
 uint8_t pushed;                     //counter for pushed symbols
 
 //decodes a 6-byte long array to a callsign
@@ -94,24 +96,28 @@ int main(void)
             dist = eucl_norm(last, pkt_sync, 8);
 
             //fprintf(stderr, "pkt_sync dist: %3.5f\n", dist);
-            if(dist<DIST_THRESH) //Frame syncword detected
+            if(dist<DIST_THRESH) //frame syncword detected
             {
                 //fprintf(stderr, "pkt_sync\n");
                 syncd=1;
                 pushed=0;
                 fl=0;
             }
-
-            //calculate euclidean norm
-            dist = eucl_norm(last, lsf_sync, 8);
-
-            //fprintf(stderr, "lsf_sync dist: %3.5f\n", dist);
-            if(dist<DIST_THRESH) //LSF syncword
+            else
             {
-                //fprintf(stderr, "lsf_sync\n");
-                syncd=1;
-                pushed=0;
-                fl=1;
+                //calculate euclidean norm again, this time against LSF syncword
+                dist = eucl_norm(last, lsf_sync, 8);
+
+                //fprintf(stderr, "lsf_sync dist: %3.5f\n", dist);
+                if(dist<DIST_THRESH) //LSF syncword
+                {
+                    //fprintf(stderr, "lsf_sync\n");
+                    syncd=1;
+                    pushed=0;
+                    last_fn=-1;
+                    memset(packet_data, 0, 33*25);
+                    fl=1;
+                }
             }
         }
         else
@@ -176,25 +182,48 @@ int main(void)
                 if(!fl)
                 {
                     //decode
+                    #ifdef SHOW_VITERBI_ERRS
                     uint32_t e=decodePunctured(frame_data, d_soft_bit, P_3, SYM_PER_PLD*2, 8);
+                    #else
+                    decodePunctured(frame_data, d_soft_bit, P_3, SYM_PER_PLD*2, 8);
+                    #endif
 
-                    //dump data - first byte is empty
-                    if(frame_data[1]==0x05) //if text message (TODO: check its length)
+                    //dump FN
+                    uint8_t rx_fn=(frame_data[26]>>2)&0x1F;
+                    uint8_t rx_last=frame_data[26]>>7;
+                    //fprintf(stderr, "FN%d, (%d)\n", rx_fn, rx_last);
+
+                    //copy data - might require some fixing
+                    if(rx_fn<=31 && rx_fn==last_fn+1 && !rx_last)
                     {
-                        fprintf(stderr, "%s", &frame_data[2]);
+                        memcpy(&packet_data[rx_fn*25], &frame_data[1], 25);
+                        last_fn++;
                     }
-                    else
+                    else if(rx_last)
                     {
-                        fprintf(stderr, "PKT: ");
-                        for(uint8_t i=1; i<27; i++)
+                        memcpy(&packet_data[last_fn*25], &frame_data[1], rx_fn);
+
+                        //dump data
+                        if(packet_data[0]==0x05) //if a text message
                         {
-                            fprintf(stderr, "%02X", frame_data[i]);
+                            fprintf(stderr, "%s", &packet_data[1]);
                         }
+                        else
+                        {
+                            fprintf(stderr, "PKT: ");
+                            for(uint16_t i=0; i<last_fn*25+rx_fn; i++)
+                            {
+                                fprintf(stderr, "%02X", packet_data[i]);
+                            }
+                        }
+
+                        fprintf(stderr, "\n");
                     }
+
                     #ifdef SHOW_VITERBI_ERRS
                     fprintf(stderr, " e=%1.1f\n", (float)e/0xFFFF);
                     #else
-                    fprintf(stderr, "\n");
+                    //fprintf(stderr, "\n");
                     #endif
 
                     //send codec2 stream to stdout
@@ -205,7 +234,11 @@ int main(void)
                     //fprintf(stderr, "LSF\n");
 
                     //decode
+                    #ifdef SHOW_VITERBI_ERRS
                     uint32_t e=decodePunctured(lsf, d_soft_bit, P_1, 2*SYM_PER_PLD, 61);
+                    #else
+                    decodePunctured(lsf, d_soft_bit, P_1, 2*SYM_PER_PLD, 61);
+                    #endif
 
                     //shift the buffer 1 position left - get rid of the encoded flushing bits
                     for(uint8_t i=0; i<30; i++)
