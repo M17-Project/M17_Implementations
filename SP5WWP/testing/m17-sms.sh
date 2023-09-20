b52d1c1fd01df7586c37ae938f9ae5b7aed06460
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2023-09-20 09:37:55 +0200

    baseband generating script update
    added support for UTF-8 encoded text

--- SP5WWP/testing/m17-sms.sh
+++ SP5WWP/testing/m17-sms.sh
@@ -9,6 +9,7 @@ then
 fi
 
 str=$4
-len=$((${#str}+2)) #add the 2 additional bytes (0x05 and 0x00)
+printf -v _ %s%n "$str" len #get UTF-8 encoded length
+len=$(($len+2)) #add the 2 additional bytes (0x05 and 0x00)
 echo -en "\x05"$str"\x00" | m17-packet-encode -S $2 -D $1 -C $3 -n $len -o $5
 sox -t raw -r 48000 -b 16 -c 1 -L -e signed-integer $5 $5".wav"
