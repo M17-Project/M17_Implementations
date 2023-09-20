06dd06ea80102cc7c337fc4e51929db2b7b5c1f9
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2023-09-20 08:59:31 +0200

    `m17-sms.sh` script addition
    script for generating M17 text message packet baseband as a WAVE file

---
+++ SP5WWP/testing/m17-sms.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -lt 5 ]
+then
+	echo "Not enough parameters."
+	echo "Usage: sh m17-sms.sh dst src can message output_file"
+	echo "Exiting."
+	exit
+fi
+
+str=$4
+len=$((${#str}+2)) #add the 2 additional bytes (0x05 and 0x00)
+echo -en "\x05"$str"\x00" | m17-packet-encode -S $2 -D $1 -C $3 -n $len -o $5
+sox -t raw -r 48000 -b 16 -c 1 -L -e signed-integer $5 $5".wav"
